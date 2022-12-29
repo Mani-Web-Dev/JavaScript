d500570c9f8d852f5c14b95761245553aebbed5b
Manigandan H <47908708+Mani-Web-Dev@users.noreply.github.com>
2022-12-29 23:42:08 +0530

    .

--- Projects/TodoList/TodoList.js
+++ Projects/TodoList/TodoList.js
@@ -1,35 +1,39 @@
 var todoForm = document.querySelector("#todoForm");
-console.log(todoForm);
-
 var textInput = document.querySelector("#textInput");
-
 var listData = document.querySelector("#listData");
+var noData = document.querySelector(".no-data");
+
 
 // On submitting the form
 
-var todoList = ["Item 1", "Item2", "Item"]
-
+var todoList = [];
 
 function displayData() {
-    for (listItem of todoList) {
-        listData.innerHTML = `
-            <li>
-                <span>${listItem}</span>
-                <button class="list-button edit"><i class="fa-solid fa-pen-to-square"></i> Edit</button>
-                <button class="list-button delete"><i class="fa-solid fa-trash-can"></i> Delete</button>
-            </li>
-        `
+    if (todoList.length === 0) {
+        noData.style.display = "block";
+    } else {
+        noData.style.display = "none";
+        for (var i = 0; i < todoList.length; i++)
+            listData.innerHTML += `<li>
+            <span><span class="index">${i + 1}. </span>${todoList[i]}</span>
+            <button class="list-button edit"><i class="fa-solid fa-pen-to-square"></i> Edit</button>
+            <button class="list-button delete"><i class="fa-solid fa-trash-can"></i> Delete</button>
+        </li>`
     }
 }
-displayData();
 
+displayData();
 
 todoForm.addEventListener("submit", (event) => {
     event.preventDefault();
-
     todoList.push(textInput.value);
+    noData.style.display = "none";
     textInput.value = "";
-    displayData();
+    listData.innerHTML += `<li>
+            <span><span class="index">${todoList.length - 1}. </span>${todoList[todoList.length - 1]}</span>
+            <button class="list-button edit"><i class="fa-solid fa-pen-to-square"></i> Edit</button>
+            <button class="list-button delete"><i class="fa-solid fa-trash-can"></i> Delete</button>
+        </li>`
 });
 
 
