cdf7fe851e37afbdb3c98b7dea3a6f4f84fd2321
Manigandan H <47908708+Mani-Web-Dev@users.noreply.github.com>
2022-12-29 13:47:43 +0530

    ....

---
+++ Exercises/MiniProjects/Calculator/Calculator.js
@@ -0,0 +1,31 @@
+
+
+var buttons = document.querySelectorAll(".button");
+var input = document.querySelector(".calc-input");
+var output = document.querySelector(".calc-output");
+var ce = document.querySelector(".CE");
+var backspace = document.querySelector(".backspace");
+var equals = document.querySelector(".equals");
+
+for (button of buttons) {
+    button.addEventListener("click", (event) => {
+        event.target.className === "button" ?
+            input.innerHTML += event.target.innerHTML :
+            null;
+    });
+}
+ce.addEventListener("click", () => {
+    input.innerHTML = "";
+    output.value = ""
+}
+);
+backspace.addEventListener("click", () => {
+    var strArray = input.innerHTML.split("");
+    strArray.pop()
+    input.innerHTML = strArray.join("");
+})
+
+equals.addEventListener("click", () => {
+    var result = eval(input.innerHTML);
+    output.value = result;
+})
