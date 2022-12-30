61e4757b77b002cbf75b4de9adeefc95ab29d9db
Manigandan H <47908708+Mani-Web-Dev@users.noreply.github.com>
2022-12-30 22:03:42 +0530

    .

--- Projects/TodoList/TodoList.js
+++ Projects/TodoList/TodoList.js
@@ -3,22 +3,77 @@ var textInput = document.querySelector("#textInput");
 var listData = document.querySelector("#listData");
 var noData = document.querySelector(".no-data");
 
+function listItemHTML(task, id) {
+    return `
+        <li id="list-item-${id + 1}">
+            <span><span class="index">${id + 1}. </span>${task}</span>
+            <button class="list-button edit" onclick=""><i class="fa-solid fa-pen-to-square"></i> Edit</button>
+            <button class="list-button delete" onclick=""><i class="fa-solid fa-trash-can"></i> Delete</button>
+        </li>
+        `
+}
+
 
 // On submitting the form
 
+var inputVal;
+
+textInput.addEventListener("change", (e) => {
+    inputVal = e.target.value;
+});
 
 
-localStorage.setItem("todoList", [])
-var todoList = localStorage.todoList.split("");
+
+
+
+
+
+todoForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+
+    localStorage.setItem("todoList", function () {
+        return inputVal;
+    }());
+
+    console.log(todoList)
+
+    textInput.value = "";
+})
+
+
 
 
 // Reading the array object form the local storage database
 
-function readData() {
-    console.log("function working")
+function displayData() {
+    if (todoList.length === 0 || todoList[0] == "") {
+        listData.innerHTML = `<p class="no-data">You haven't created any tasks yet</p>`
+    } else {
+        todoList.map(function (item, index) {
+            listData.innerHTML += listItemHTML(item, index);
+        })
+    }
 }
+displayData();
 
-console.log(todoList);
+
+function createData() {
+    console.log("createData");
+}
+createData();
+
+
+function readData() {
+    console.log("readData");
+}
+readData();
+
+
+function deleteData() {
+    console.log("deleteData");
+}
+deleteData();
 
 
 
