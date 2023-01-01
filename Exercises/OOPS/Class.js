5269320c9169b5e8cd51e8bfa369e0646fc49ac8
Manigandan H <47908708+Mani-Web-Dev@users.noreply.github.com>
2023-01-01 16:01:21 +0530

    ..

--- Exercises/OOPS/Class.js
+++ Exercises/OOPS/Class.js
@@ -49,6 +49,51 @@ console.log(E1);
 
 
 
+// ---------------------------------------------------------
+
+/* 
+
+OOPS Features
+
+-> Abstraction
+-> Encapsulation
+-> Inheritance                       
+  
+
+-> Polymorphism
+
+
+Inheritance 
+
+reusing a existing feature or a property/method
+
+adding new functionality 
+
+it even inherits the constructor methods as well
+
+this can be achieved using extends keyword in javascript
+
+we can reuse as many times as we want
+
+we can override the methods also 
+
+
+
+Tasks : please try an example using extends keyword
+
+if we did not have a constructor in class, it has default c"
+
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++onstructor
+
+*/[]
+g                                                                                                                                
+
+
+
+
+
+
+
 
 
 
