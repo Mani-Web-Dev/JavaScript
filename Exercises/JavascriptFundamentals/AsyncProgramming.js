e2871ce6794c6c4b595144e32912691948eed4b4
Manigandan H <47908708+Mani-Web-Dev@users.noreply.github.com>
2023-01-08 18:21:22 +0530

    g

---
+++ Exercises/JavascriptFundamentals/AsyncProgramming.js
@@ -0,0 +1,11 @@
+/*
+
+Async Programming
+
+setTimeOut(); => excecutes a function after a certian time interval
+SetInterval(); => executes a function every after a certian interval repeatedly
+
+
+This is non blocking technique
+
+*/ 
