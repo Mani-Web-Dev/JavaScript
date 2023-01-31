3fb929633e38976c57293b5c7d925b89e09b8a9e
Manigandan H <47908708+Mani-Web-Dev@users.noreply.github.com>
2023-01-31 23:42:03 +0530

    Update TodoList.js

--- Projects/TodoList/TodoList.js
+++ Projects/TodoList/TodoList.js
@@ -7,7 +7,6 @@ var JsonPreview = document.querySelector("#JsonPreview");
 
 // On submitting the form
 
-
 var localStorageArray = JSON.parse(localStorage.getItem("todoListData"));
 
 if (localStorageArray) {
@@ -17,43 +16,38 @@ if (localStorageArray) {
 
 var getData = localStorageArray;
 
-// function createElement(arr) {
-//     arr.map((item) => {
-//         var listNode = document.createElement("li");
-//         var deleteButton = document.createElement("button");
-//         deleteButton.innerText = "Delete";
-//         deleteButton.className = "bg-red-500 text-white p-2 rounded-full hover:bg-red-600 ml-auto";
-//         deleteButton.addEventListener("click", () => {
-//             console.log(item);
-//         })
-//         listNode.innerText = item.id + ") " + item.taskItem;
-//         listNode.className = "border border-gray-400 p-2 my-2 flex items-center";
-//         ListData.appendChild(listNode);
-//         listNode.appendChild(deleteButton);
-//     })
-// }
-// createElement(getData);
+function createElement(arr) {
+    arr.map((item) => {
+        var listNode = document.createElement("li");
+        var deleteButton = document.createElement("button");
+        deleteButton.innerText = "Delete";
+        deleteButton.className = "bg-red-500 text-white p-2 rounded-full hover:bg-red-600 ml-auto";
+        deleteButton.addEventListener("click", () => {
+            console.log(item);
+        })
+        listNode.innerText = item.id + ") " + item.taskItem;
+        listNode.className = "border border-gray-400 p-2 my-2 flex items-center";
+        ListData.appendChild(listNode);
+        listNode.appendChild(deleteButton);
+    })
+}
+createElement(getData);
 
-var getData = localStorageArray;
 
 var putData;
 
 getData === null ? putData = [] : putData = [...getData]
 
-
 JsonPreview.innerText = localStorage.getItem("todoListData");
-
 
 TodoForm.addEventListener("submit", (event) => {
 
     event.preventDefault();
 
-
     putData.push({
         id: putData.length + 1,
         taskItem: TextInput.value
     });
-
 
     localStorage.setItem("todoListData", JSON.stringify(putData));
 
