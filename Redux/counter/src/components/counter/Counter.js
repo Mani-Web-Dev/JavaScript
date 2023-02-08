ecddd43fdf68d60b44a682e0d3e2d476389893e8
Manigandan H <47908708+Mani-Web-Dev@users.noreply.github.com>
2023-02-08 08:01:22 +0530

    .

--- Redux/counter/src/components/counter/Counter.js
+++ Redux/counter/src/components/counter/Counter.js
@@ -1,21 +1,41 @@
 import React from 'react';
 
+import { useSelector, useDispatch } from 'react-redux';
+
 import styles from './Counter.module.css';
 
 export function Counter() {
+
+    const counter = useSelector((state) => state.counter);
+
+    const dispatch = useDispatch();
+
+    const incrementHandler = () => {
+        dispatch({
+            type: "increment"
+        })
+    }
+    const decrementHandler = () => {
+        dispatch({
+            type: "decrement"
+        })
+    }
+
     return (
         <div>
             <div className={styles.row}>
                 <button
                     className={styles.button}
                     aria-label="Increment value"
+                    onClick={incrementHandler}
                 >
                     +
                 </button>
-                <span className={styles.value}>1</span>
+                <span className={styles.value}>{counter}</span>
                 <button
                     className={styles.button}
                     aria-label="Decrement value"
+                    onClick={decrementHandler}
                 >
                     -
                 </button>
