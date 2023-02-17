afb86683c581dab9d0640c1850c10e8b45416eec
Manigandan H <47908708+Mani-Web-Dev@users.noreply.github.com>
2023-02-17 22:51:49 +0530

    Create ConcatArrays.js

---
+++ Exercises/ConcatArrays.js
@@ -0,0 +1,12 @@
+function mergeArrays(...args) {
+    var resultArray = [];
+    for (var i = 0; i < args.length; i++) {
+        for (var j = 0; j < args[i].length; j++) {
+            resultArray.push(args[i][j]);
+        }
+    }
+    return resultArray;
+}
+
+var output = mergeArrays([1, 6], [2, 5], [3, 9], [4, 50, 50]);
+console.log(output);
